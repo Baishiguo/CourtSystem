f0d3477149e100810dc9ac7e7be33faef084ab41
bsg <2516660242@qq.com>
2022-04-02 19:47:10 +0800

    first commit

---
+++ src/main/java/nanjing/hhu/court/CourtApplication.java
@@ -0,0 +1,16 @@
+package nanjing.hhu.court;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: bai
+ * @create: 2022-04-02
+ **/
+@SpringBootApplication
+public class CourtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourtApplication.class, args);
+    }
+}
